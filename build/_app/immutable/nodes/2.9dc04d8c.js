9e0fce84a6176257d1222d714219d8190b708218
Mert Sefa AKGUN <git@msakg.com>
2023-10-17 18:31:06 +0300

    git(ignore): ignored build folder

--- build/_app/immutable/nodes/2.9dc04d8c.js
+++
@@ -1,1 +0,0 @@
-import{s}from"../chunks/scheduler.8da674eb.js";import{S as t,i as e}from"../chunks/index.12812435.js";class l extends t{constructor(o){super(),e(this,o,null,null,s,{})}}export{l as component};
