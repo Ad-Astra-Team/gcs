231791144b9671de68b9d369063ddf772724ce30
Mert Sefa AKGUN <git@msakg.com>
2023-10-18 00:15:43 +0300

    init(map): leaflet map sample initialized

---
+++ src/lib/Maps/MarkerPopup.svelte
@@ -0,0 +1,28 @@
+<script>
+	import { createEventDispatcher } from 'svelte';
+	const dispatch = createEventDispatcher();
+
+	/** @type {number}*/
+	export let count;
+
+	/** @param {number} delta */
+	function addValue(delta) {
+		count += delta;
+		dispatch('change', count);
+	}
+</script>
+
+<div>
+	<button on:click={() => addValue(-1)}> - </button>
+	<button on:click={() => addValue(1)}> + </button>
+</div>
+
+<div style="width:100%;text-align:center;font-weight:600">
+	{count}
+</div>
+
+<style>
+	button {
+		width: 2rem;
+	}
+</style>
--- src/routes/test/+page.svelte
+++ src/routes/test/+page.svelte
@@ -1,1 +1,11 @@
-<div>Test Page</div>
+<script>
+	import Map from '$lib/Maps/Map.svelte';
+</script>
+
+<div class="container min-w-full min-h-full">
+	<div class="grid grid-cols-3">
+		<div />
+		<Map />
+		<div />
+	</div>
+</div>
